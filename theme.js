bcd52183e98ae3b325c1b7937a110429d44a46e4
Pavlos Vinieratos <pvinis@gmail.com>
2019-07-03 23:49:47 +0200

    more

--- theme.js
+++ theme.js
@@ -1,8 +1,25 @@
-export default {
-    monospace: 'Iosevka',
+const base = {
+    monospace: 'Iosevka, Menlo',
+}
+
+export const eightiesDark = {
+    ...base,
+
     colors: {
-        text: 'red',
-        background: 'blue',
-        link: 'green',
+        text: '#d3d0c8',
+        background: '#2d2d2d',
+        // caret: d3d0c8
+        // background: 515151
+        // foreground: 6699cc
+        // foreground: 66cccc
+        // foreground: 747369
+        // foreground: 99cc99
+        // foreground: a09f93
+        // foreground: cc99cc
+        // foreground: d27b53
+        // foreground: d3d0c8
+        // foreground: f2777a
+        // foreground: f99157
+        // foreground: ffcc66
     },
 }
