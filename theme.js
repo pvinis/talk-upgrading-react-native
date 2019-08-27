f7861e87f6ddfdfed48caedea1aafd5a5ce4d803
Pavlos Vinieratos <pvinis@gmail.com>
2019-08-27 23:35:05 +0200

    progress

--- theme.js
+++ theme.js
@@ -1,26 +1,43 @@
 const blue = '#3AD'
 
+
 const accent = blue
 const background = '#222'
+const text = '#aaa'
+
 
 export const myTheme = {
     fonts: {
+        heading: 'Iosevka Web',
         body: 'Iosevka Web',
+        monospace: 'Iosevka Web',
     },
     colors: {
-      text: accent,
-      background,
-      primary: '#08f',
-      secondary: '#f0f',
-      darkgray: '#333',
+        text,
+        background,
+        primary: 'red',
     },
     styles: {
-      pre: {
-        color: 'secondary',
-        bg: 'darkgray',
-      },
-      code: {
-        color: 'secondary',
-      },
+        root: {
+            fontFamily: 'Iosevka Web',
+            fontWeight: 300, // light
+        },
+        h1: {
+            color: accent,
+            fontWeight: 700, // bold
+        },
+        h2: {
+            color: accent,
+        },
+        inlineCode: {
+            color: accent,
+        },
+        code: {
+            color: accent,
+            fontSize: '30px',
+        },
+        p: {
+            textAlign: 'center',
+        },
     },
-  }
+}
