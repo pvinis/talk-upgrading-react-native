d51a81fcd6a718cb71f15d6cce45ab0c2d3b6497
Pavlos Vinieratos <pvinis@gmail.com>
2019-08-27 22:49:00 +0200

    works

---
+++ theme.js
@@ -0,0 +1,26 @@
+const blue = '#3AD'
+
+const accent = blue
+const background = '#222'
+
+export const myTheme = {
+    fonts: {
+        body: 'Iosevka Web',
+    },
+    colors: {
+      text: accent,
+      background,
+      primary: '#08f',
+      secondary: '#f0f',
+      darkgray: '#333',
+    },
+    styles: {
+      pre: {
+        color: 'secondary',
+        bg: 'darkgray',
+      },
+      code: {
+        color: 'secondary',
+      },
+    },
+  }
